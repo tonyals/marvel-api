ea7ddfbfe6270acec812091c76c73fa4bd99dd32
Tony Augusto <tony_luzsilva@hotmail.com>
2021-05-31 20:21:10 -0300

    feat: handle exception

--- src/main/java/br/com/tony/marvelapi/resource/handler/RestExceptionHandler.java
+++ src/main/java/br/com/tony/marvelapi/resource/handler/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.tony.marvelapi.resource.handler;
 
 import br.com.tony.marvelapi.exception.BusinessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,11 +9,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class RestExceptionHandler {
 
+    private static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal Error";
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorMessage> handleBusinessException(BusinessException e) {
         return new ResponseEntity<>(ErrorMessage.builder()
                 .message(e.getMessage())
                 .statusCode(e.getHttpStatus().value())
                 .build(), e.getHttpStatus());
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<ErrorMessage> handleInternalException(Throwable e) {
+        return new ResponseEntity<>(ErrorMessage.builder()
+                .message(INTERNAL_SERVER_ERROR_MESSAGE)
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
